fee70d034598bcbf79c0d061ef8f948331e50def
Enrique Bravo <patonews115gmail.com>
2022-07-12 17:09:22 -0500

    Add project files.

---
+++ CwkBooking.Api/Controllers/HotelsController.cs
@@ -0,0 +1,78 @@
+﻿using CwkBooking.Dal;
+using CwkBooking.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CwkBooking.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HotelsController : Controller
+    {
+        private readonly ILogger<HotelsController> _logger;
+        private readonly HttpContext _http;
+        private readonly DataContext _ctx;
+        public HotelsController(ILogger<HotelsController> logger, IHttpContextAccessor httpContextAccessor,
+            DataContext ctx)
+        {
+            _logger = logger;
+            _http = httpContextAccessor.HttpContext;
+            _ctx = ctx;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllHotels()
+        {
+            var hotels = await _ctx.Hotels.ToListAsync();
+            return Ok(hotels);
+        }
+
+
+        [Route("{id}")]
+        [HttpGet]
+        public async Task<IActionResult> GetHotelById(int id)
+        {
+            var hotel = await _ctx.Hotels.FirstOrDefaultAsync(h => h.HotelId == id);
+            return Ok(hotel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateHotel([FromBody] Hotel hotel)
+        {
+            _ctx.Hotels.Add(hotel);
+            await _ctx.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetHotelById), new { id = hotel.HotelId }, hotel);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> UpdateHotel([FromBody] Hotel updated, int id)
+        {
+            var hotel = await _ctx.Hotels.FirstOrDefaultAsync(h => h.HotelId == id);
+            hotel.Stars = updated.Stars;
+            hotel.Description = updated.Description;
+            hotel.Name = updated.Name;
+
+            _ctx.Hotels.Update(hotel);
+            await _ctx.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteHotel(int id)
+        {
+            var hotel = await _ctx.Hotels.FirstOrDefaultAsync(h => h.HotelId == id);
+            _ctx.Hotels.Remove(hotel);
+            await _ctx.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
